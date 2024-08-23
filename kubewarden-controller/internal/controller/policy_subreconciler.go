kubewarden/kubewarden-controller

feat: policy groups controllers.
Adds new controllers to reconcile the policy groups CRDs. These
controllers reuse the same code from the previous policies controllers.

Signed-off-by: José Guilherme Vanz <jguilhermevanz@suse.com>

internal/controller/policy_subreconciler.go
@@ -31,6 +31,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/go-logr/logr"
 	policiesv1 "github.com/kubewarden/kubewarden-controller/api/policies/v1"
@@ -52,7 +53,7 @@ func (r *policySubReconciler) reconcile(ctx context.Context, policy policiesv1.P
 
 	reconcileResult, reconcileErr := r.reconcilePolicy(ctx, policy)
 
-	if err := r.setPolicyStatus(ctx, policy); err != nil {
+	if err := r.setPolicyModeStatus(ctx, policy); err != nil {
 		return ctrl.Result{}, fmt.Errorf("error setting policy status: %w", err)
 	}
 
@@ -166,7 +167,7 @@ func (r *policySubReconciler) reconcilePolicyDeletion(ctx context.Context, polic
 	return ctrl.Result{}, nil
 }
 
-func (r *policySubReconciler) setPolicyStatus(ctx context.Context, policy policiesv1.Policy) error {
+func (r *policySubReconciler) setPolicyModeStatus(ctx context.Context, policy policiesv1.Policy) error {
 	policyServerDeployment := appsv1.Deployment{}
 	policyServerDeploymentName := policyServerDeploymentName(policy.GetPolicyServer())
 
@@ -277,6 +278,180 @@ func isPodReady(pod corev1.Pod) bool {
 		}
 	}
 	return false
+}
+
+func findAdmissionPoliciesForConfigMap(object client.Object) []reconcile.Request {
+	configMap, ok := object.(*corev1.ConfigMap)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	if _, isKubewardenConfigmap := configMap.Labels[constants.PolicyServerLabelKey]; !isKubewardenConfigmap {
+		return []reconcile.Request{}
+	}
+	policyMap, err := getPolicyMapFromConfigMap(configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return policyMap.toAdmissionPolicyReconcileRequests()
+}
+
+func findClusterAdmissionPoliciesForConfigMap(object client.Object) []reconcile.Request {
+	configMap, ok := object.(*corev1.ConfigMap)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyMap, err := getPolicyMapFromConfigMap(configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return policyMap.toClusterAdmissionPolicyReconcileRequests()
+}
+
+func findAdmissionPoliciesForPod(ctx context.Context, k8sClient client.Client, object client.Object) []reconcile.Request {
+	pod, ok := object.(*corev1.Pod)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerName, isKubewardenPod := pod.Labels[constants.PolicyServerLabelKey]
+	if !isKubewardenPod || pod.DeletionTimestamp != nil {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServerName)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: pod.ObjectMeta.Namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findAdmissionPoliciesForConfigMap(&configMap)
+}
+
+func findClusterAdmissionPoliciesForPod(ctx context.Context, k8sClient client.Client, object client.Object) []reconcile.Request {
+	pod, ok := object.(*corev1.Pod)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerName, ok := pod.Labels[constants.PolicyServerLabelKey]
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServerName)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: pod.ObjectMeta.Namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findClusterAdmissionPoliciesForConfigMap(&configMap)
+}
+
+func findClusterAdmissionPoliciesForPolicyServer(ctx context.Context, k8sClient client.Client, object client.Object, namespace string) []reconcile.Request {
+	policyServer, ok := object.(*policiesv1.PolicyServer)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServer.Name)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findClusterAdmissionPoliciesForConfigMap(&configMap)
+}
+
+func findClusterAdmissionPolicyForWebhookConfiguration(webhookConfiguration client.Object, log logr.Logger) []reconcile.Request {
+	if _, found := webhookConfiguration.GetLabels()["kubewarden"]; !found {
+		return []reconcile.Request{}
+	}
+
+	policyScope, found := webhookConfiguration.GetLabels()[constants.WebhookConfigurationPolicyScopeLabelKey]
+	if !found {
+		log.Info("Found a webhook configuration without a scope label, reconciling it",
+			"name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	// Filter out AdmissionPolicies
+	if policyScope != constants.ClusterPolicyScope {
+		return []reconcile.Request{}
+	}
+
+	policyName, found := webhookConfiguration.GetAnnotations()[constants.WebhookConfigurationPolicyNameAnnotationKey]
+	if !found {
+		log.Info("Found a webhook configuration without a policy name annotation, reconciling it",
+			"name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	return []reconcile.Request{
+		{
+			NamespacedName: client.ObjectKey{
+				Name: policyName,
+			},
+		},
+	}
+}
+
+func findAdmissionPoliciesForPolicyServer(ctx context.Context, k8sClient client.Client, object client.Object, namespace string) []reconcile.Request {
+	policyServer, ok := object.(*policiesv1.PolicyServer)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServer.Name)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findAdmissionPoliciesForConfigMap(&configMap)
+}
+
+func findAdmissionPolicyForWebhookConfiguration(webhookConfiguration client.Object, log logr.Logger) []reconcile.Request {
+	if _, found := webhookConfiguration.GetLabels()["kubewarden"]; !found {
+		return []reconcile.Request{}
+	}
+
+	policyScope, found := webhookConfiguration.GetLabels()[constants.WebhookConfigurationPolicyScopeLabelKey]
+	if !found {
+		log.Info("Found a webhook configuration without a scope label, reconciling it", "name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	// Filter out ClusterAdmissionPolicies
+	if policyScope != constants.NamespacePolicyScope {
+		return []reconcile.Request{}
+	}
+
+	policyNamespace, found := webhookConfiguration.GetAnnotations()[constants.WebhookConfigurationPolicyNamespaceAnnotationKey]
+	if !found {
+		log.Info("Found a webhook configuration without a namespace annotation, reconciling it", "name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	policyName, found := webhookConfiguration.GetAnnotations()[constants.WebhookConfigurationPolicyNameAnnotationKey]
+	if !found {
+		log.Info("Found a webhook configuration without a policy name annotation, reconciling it", "name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	return []reconcile.Request{
+		{
+			NamespacedName: client.ObjectKey{
+				Name:      policyName,
+				Namespace: policyNamespace,
+			},
+		},
+	}
 }
 
 func getPolicyMapFromConfigMap(configMap *corev1.ConfigMap) (policyConfigEntryMap, error) {

internal/controller/utils_test.go
@@ -65,6 +65,20 @@ var (
 			},
 		},
 	}
+	templateAdmissionPolicyGroup = policiesv1.AdmissionPolicyGroup{
+		Spec: policiesv1.AdmissionPolicyGroupSpec{
+			PolicyGroupSpec: policiesv1.PolicyGroupSpec{
+				Rules: []admissionregistrationv1.RuleWithOperations{},
+			},
+		},
+	}
+	templateClusterAdmissionPolicyGroup = policiesv1.ClusterAdmissionPolicyGroup{
+		Spec: policiesv1.ClusterAdmissionPolicyGroupSpec{
+			PolicyGroupSpec: policiesv1.PolicyGroupSpec{
+				Rules: []admissionregistrationv1.RuleWithOperations{},
+			},
+		},
+	}
 )
 
 func policyServerVersion() string {
@@ -115,6 +129,59 @@ func admissionPolicyFactory(name, policyNamespace, policyServerName string, muta
 	return admissionPolicy
 }
 
+func admissionPolicyGroupFactory(name, policyNamespace, policyServerName string) *policiesv1.AdmissionPolicyGroup {
+	admissionPolicy := templateAdmissionPolicyGroup.DeepCopy()
+	admissionPolicy.Name = name
+	admissionPolicy.Namespace = policyNamespace
+	admissionPolicy.Spec.PolicyServer = policyServerName
+	admissionPolicy.Spec.PolicyGroupSpec.MatchConditions = []admissionregistrationv1.MatchCondition{
+		{
+			Name:       "noop",
+			Expression: "true",
+		},
+	}
+	admissionPolicy.Spec.Policies = []policiesv1.PolicyGroupMember{{
+		Name: "pod-privileged",
+		URL:  "registry://ghcr.io/kubewarden/tests/pod-privileged:v0.2.5",
+	}}
+	admissionPolicy.Finalizers = []string{
+		// On a real cluster the Kubewarden finalizer is added by our mutating
+		// webhook. This is not running now, hence we have to manually add the finalizer
+		constants.KubewardenFinalizer,
+		// By adding this finalizer automatically, we ensure that when
+		// testing removal of finalizers on deleted objects, that they will
+		// exist at all times
+		integrationTestsFinalizer,
+	}
+	return admissionPolicy
+}
+
+func clusterAdmissionPolicyGroupFactory(name, policyServerName string) *policiesv1.ClusterAdmissionPolicyGroup {
+	clusterAdmissionPolicy := templateClusterAdmissionPolicyGroup.DeepCopy()
+	clusterAdmissionPolicy.Name = name
+	clusterAdmissionPolicy.Spec.PolicyServer = policyServerName
+	clusterAdmissionPolicy.Spec.PolicyGroupSpec.MatchConditions = []admissionregistrationv1.MatchCondition{
+		{
+			Name:       "noop",
+			Expression: "true",
+		},
+	}
+	clusterAdmissionPolicy.Finalizers = []string{
+		// On a real cluster the Kubewarden finalizer is added by our mutating
+		// webhook. This is not running now, hence we have to manually add the finalizer
+		constants.KubewardenFinalizer,
+		// By adding this finalizer automatically, we ensure that when
+		// testing removal of finalizers on deleted objects, that they will
+		// exist at all times
+		integrationTestsFinalizer,
+	}
+	clusterAdmissionPolicy.Spec.Policies = []policiesv1.PolicyGroupMember{{
+		Name: "pod-privileged",
+		URL:  "registry://ghcr.io/kubewarden/tests/pod-privileged:v0.2.5",
+	}}
+	return clusterAdmissionPolicy
+}
+
 func clusterAdmissionPolicyFactory(name, policyServerName string, mutating bool) *policiesv1.ClusterAdmissionPolicy {
 	clusterAdmissionPolicy := templateClusterAdmissionPolicy.DeepCopy()
 	clusterAdmissionPolicy.Name = name
@@ -146,12 +213,28 @@ func getTestAdmissionPolicy(ctx context.Context, namespace, name string) (*polic
 	return &admissionPolicy, nil
 }
 
+func getTestAdmissionPolicyGroup(ctx context.Context, namespace, name string) (*policiesv1.AdmissionPolicyGroup, error) {
+	admissionPolicyGroup := policiesv1.AdmissionPolicyGroup{}
+	if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: name}, &admissionPolicyGroup); err != nil {
+		return nil, errors.Join(errors.New("could not find AdmissionPolicyGroup"), err)
+	}
+	return &admissionPolicyGroup, nil
+}
+
 func getTestClusterAdmissionPolicy(ctx context.Context, name string) (*policiesv1.ClusterAdmissionPolicy, error) {
 	clusterAdmissionPolicy := policiesv1.ClusterAdmissionPolicy{}
 	if err := k8sClient.Get(ctx, client.ObjectKey{Name: name}, &clusterAdmissionPolicy); err != nil {
 		return nil, errors.Join(errors.New("could not find ClusterAdmissionPolicy"), err)
 	}
 	return &clusterAdmissionPolicy, nil
+}
+
+func getTestClusterAdmissionPolicyGroup(ctx context.Context, name string) (*policiesv1.ClusterAdmissionPolicyGroup, error) {
+	clusterAdmissionPolicyGroup := policiesv1.ClusterAdmissionPolicyGroup{}
+	if err := k8sClient.Get(ctx, client.ObjectKey{Name: name}, &clusterAdmissionPolicyGroup); err != nil {
+		return nil, errors.Join(errors.New("could not find ClusterAdmissionPolicyGroup"), err)
+	}
+	return &clusterAdmissionPolicyGroup, nil
 }
 
 func getTestPolicyServer(ctx context.Context, name string) (*policiesv1.PolicyServer, error) {

jvanz/kubewarden-filter-repo-testing

kubewarden-controller/internal/controller/policy_subreconciler.go
@@ -31,6 +31,7 @@ import (
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	"github.com/go-logr/logr"
 	policiesv1 "github.com/kubewarden/kubewarden-controller/api/policies/v1"
@@ -52,7 +53,7 @@ func (r *policySubReconciler) reconcile(ctx context.Context, policy policiesv1.P
 
 	reconcileResult, reconcileErr := r.reconcilePolicy(ctx, policy)
 
-	if err := r.setPolicyStatus(ctx, policy); err != nil {
+	if err := r.setPolicyModeStatus(ctx, policy); err != nil {
 		return ctrl.Result{}, fmt.Errorf("error setting policy status: %w", err)
 	}
 
@@ -166,7 +167,7 @@ func (r *policySubReconciler) reconcilePolicyDeletion(ctx context.Context, polic
 	return ctrl.Result{}, nil
 }
 
-func (r *policySubReconciler) setPolicyStatus(ctx context.Context, policy policiesv1.Policy) error {
+func (r *policySubReconciler) setPolicyModeStatus(ctx context.Context, policy policiesv1.Policy) error {
 	policyServerDeployment := appsv1.Deployment{}
 	policyServerDeploymentName := policyServerDeploymentName(policy.GetPolicyServer())
 
@@ -277,6 +278,180 @@ func isPodReady(pod corev1.Pod) bool {
 		}
 	}
 	return false
+}
+
+func findPoliciesForConfigMap(object client.Object) []reconcile.Request {
+	configMap, ok := object.(*corev1.ConfigMap)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	if _, isKubewardenConfigmap := configMap.Labels[constants.PolicyServerLabelKey]; !isKubewardenConfigmap {
+		return []reconcile.Request{}
+	}
+	policyMap, err := getPolicyMapFromConfigMap(configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return policyMap.toAdmissionPolicyReconcileRequests()
+}
+
+func findClusterPoliciesForConfigMap(object client.Object) []reconcile.Request {
+	configMap, ok := object.(*corev1.ConfigMap)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyMap, err := getPolicyMapFromConfigMap(configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return policyMap.toClusterAdmissionPolicyReconcileRequests()
+}
+
+func findPoliciesForPod(ctx context.Context, k8sClient client.Client, object client.Object) []reconcile.Request {
+	pod, ok := object.(*corev1.Pod)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerName, isKubewardenPod := pod.Labels[constants.PolicyServerLabelKey]
+	if !isKubewardenPod || pod.DeletionTimestamp != nil {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServerName)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: pod.ObjectMeta.Namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findPoliciesForConfigMap(&configMap)
+}
+
+func findClusterPoliciesForPod(ctx context.Context, k8sClient client.Client, object client.Object) []reconcile.Request {
+	pod, ok := object.(*corev1.Pod)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerName, ok := pod.Labels[constants.PolicyServerLabelKey]
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServerName)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: pod.ObjectMeta.Namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findClusterPoliciesForConfigMap(&configMap)
+}
+
+func findClusterPoliciesForPolicyServer(ctx context.Context, k8sClient client.Client, object client.Object, namespace string) []reconcile.Request {
+	policyServer, ok := object.(*policiesv1.PolicyServer)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServer.Name)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findClusterPoliciesForConfigMap(&configMap)
+}
+
+func findClusterPolicyForWebhookConfiguration(webhookConfiguration client.Object, log logr.Logger) []reconcile.Request {
+	if _, found := webhookConfiguration.GetLabels()["kubewarden"]; !found {
+		return []reconcile.Request{}
+	}
+
+	policyScope, found := webhookConfiguration.GetLabels()[constants.WebhookConfigurationPolicyScopeLabelKey]
+	if !found {
+		log.Info("Found a webhook configuration without a scope label, reconciling it",
+			"name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	// Filter out AdmissionPolicies
+	if policyScope != constants.ClusterPolicyScope {
+		return []reconcile.Request{}
+	}
+
+	policyName, found := webhookConfiguration.GetAnnotations()[constants.WebhookConfigurationPolicyNameAnnotationKey]
+	if !found {
+		log.Info("Found a webhook configuration without a policy name annotation, reconciling it",
+			"name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	return []reconcile.Request{
+		{
+			NamespacedName: client.ObjectKey{
+				Name: policyName,
+			},
+		},
+	}
+}
+
+func findPoliciesForPolicyServer(ctx context.Context, k8sClient client.Client, object client.Object, namespace string) []reconcile.Request {
+	policyServer, ok := object.(*policiesv1.PolicyServer)
+	if !ok {
+		return []reconcile.Request{}
+	}
+	policyServerDeploymentName := policyServerDeploymentName(policyServer.Name)
+	configMap := corev1.ConfigMap{}
+	err := k8sClient.Get(ctx, client.ObjectKey{
+		Namespace: namespace,
+		Name:      policyServerDeploymentName, // As the deployment name matches the name of the ConfigMap
+	}, &configMap)
+	if err != nil {
+		return []reconcile.Request{}
+	}
+	return findPoliciesForConfigMap(&configMap)
+}
+
+func findPolicyForWebhookConfiguration(webhookConfiguration client.Object, log logr.Logger) []reconcile.Request {
+	if _, found := webhookConfiguration.GetLabels()["kubewarden"]; !found {
+		return []reconcile.Request{}
+	}
+
+	policyScope, found := webhookConfiguration.GetLabels()[constants.WebhookConfigurationPolicyScopeLabelKey]
+	if !found {
+		log.Info("Found a webhook configuration without a scope label, reconciling it", "name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	// Filter out ClusterAdmissionPolicies
+	if policyScope != constants.NamespacePolicyScope {
+		return []reconcile.Request{}
+	}
+
+	policyNamespace, found := webhookConfiguration.GetAnnotations()[constants.WebhookConfigurationPolicyNamespaceAnnotationKey]
+	if !found {
+		log.Info("Found a webhook configuration without a namespace annotation, reconciling it", "name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	policyName, found := webhookConfiguration.GetAnnotations()[constants.WebhookConfigurationPolicyNameAnnotationKey]
+	if !found {
+		log.Info("Found a webhook configuration without a policy name annotation, reconciling it", "name", webhookConfiguration.GetName())
+		return []reconcile.Request{}
+	}
+
+	return []reconcile.Request{
+		{
+			NamespacedName: client.ObjectKey{
+				Name:      policyName,
+				Namespace: policyNamespace,
+			},
+		},
+	}
 }
 
 func getPolicyMapFromConfigMap(configMap *corev1.ConfigMap) (policyConfigEntryMap, error) {

kubewarden-controller/internal/controller/utils_test.go
@@ -65,6 +65,20 @@ var (
 			},
 		},
 	}
+	templateAdmissionPolicyGroup = policiesv1.AdmissionPolicyGroup{
+		Spec: policiesv1.AdmissionPolicyGroupSpec{
+			PolicyGroupSpec: policiesv1.PolicyGroupSpec{
+				Rules: []admissionregistrationv1.RuleWithOperations{},
+			},
+		},
+	}
+	templateClusterAdmissionPolicyGroup = policiesv1.ClusterAdmissionPolicyGroup{
+		Spec: policiesv1.ClusterAdmissionPolicyGroupSpec{
+			PolicyGroupSpec: policiesv1.PolicyGroupSpec{
+				Rules: []admissionregistrationv1.RuleWithOperations{},
+			},
+		},
+	}
 )
 
 func policyServerVersion() string {
@@ -115,6 +129,65 @@ func admissionPolicyFactory(name, policyNamespace, policyServerName string, muta
 	return admissionPolicy
 }
 
+func admissionPolicyGroupFactory(name, policyNamespace, policyServerName string) *policiesv1.AdmissionPolicyGroup {
+	admissionPolicy := templateAdmissionPolicyGroup.DeepCopy()
+	admissionPolicy.Name = name
+	admissionPolicy.Namespace = policyNamespace
+	admissionPolicy.Spec.PolicyServer = policyServerName
+	admissionPolicy.Spec.PolicyGroupSpec.MatchConditions = []admissionregistrationv1.MatchCondition{
+		{
+			Name:       "noop",
+			Expression: "true",
+		},
+	}
+	admissionPolicy.Spec.Policies = []policiesv1.PolicyGroupMember{{
+		Name:   "pod-privileged",
+		Module: "registry://ghcr.io/kubewarden/tests/pod-privileged:v0.2.5",
+	}}
+	admissionPolicy.Finalizers = []string{
+		// On a real cluster the Kubewarden finalizer is added by our mutating
+		// webhook. This is not running now, hence we have to manually add the finalizer
+		constants.KubewardenFinalizer,
+		// By adding this finalizer automatically, we ensure that when
+		// testing removal of finalizers on deleted objects, that they will
+		// exist at all times
+		integrationTestsFinalizer,
+	}
+	return admissionPolicy
+}
+
+func clusterAdmissionPolicyGroupFactory(name, policyServerName string) *policiesv1.ClusterAdmissionPolicyGroup {
+	clusterAdmissionPolicy := templateClusterAdmissionPolicyGroup.DeepCopy()
+	clusterAdmissionPolicy.Name = name
+	clusterAdmissionPolicy.Spec.PolicyServer = policyServerName
+	clusterAdmissionPolicy.Spec.PolicyGroupSpec.MatchConditions = []admissionregistrationv1.MatchCondition{
+		{
+			Name:       "noop",
+			Expression: "true",
+		},
+	}
+	clusterAdmissionPolicy.Finalizers = []string{
+		// On a real cluster the Kubewarden finalizer is added by our mutating
+		// webhook. This is not running now, hence we have to manually add the finalizer
+		constants.KubewardenFinalizer,
+		// By adding this finalizer automatically, we ensure that when
+		// testing removal of finalizers on deleted objects, that they will
+		// exist at all times
+		integrationTestsFinalizer,
+	}
+	clusterAdmissionPolicy.Spec.Policies = []policiesv1.PolicyGroupMember{
+		{
+			Name:   "pod-privileged",
+			Module: "registry://ghcr.io/kubewarden/tests/pod-privileged:v0.2.5",
+		},
+		{
+			Name:   "user-group-psp",
+			Module: "registry://ghcr.io/kubewarden/tests/user-group-psp:v0.4.9",
+		},
+	}
+	return clusterAdmissionPolicy
+}
+
 func clusterAdmissionPolicyFactory(name, policyServerName string, mutating bool) *policiesv1.ClusterAdmissionPolicy {
 	clusterAdmissionPolicy := templateClusterAdmissionPolicy.DeepCopy()
 	clusterAdmissionPolicy.Name = name
@@ -146,12 +219,28 @@ func getTestAdmissionPolicy(ctx context.Context, namespace, name string) (*polic
 	return &admissionPolicy, nil
 }
 
+func getTestAdmissionPolicyGroup(ctx context.Context, namespace, name string) (*policiesv1.AdmissionPolicyGroup, error) {
+	admissionPolicyGroup := policiesv1.AdmissionPolicyGroup{}
+	if err := k8sClient.Get(ctx, client.ObjectKey{Namespace: namespace, Name: name}, &admissionPolicyGroup); err != nil {
+		return nil, errors.Join(errors.New("could not find AdmissionPolicyGroup"), err)
+	}
+	return &admissionPolicyGroup, nil
+}
+
 func getTestClusterAdmissionPolicy(ctx context.Context, name string) (*policiesv1.ClusterAdmissionPolicy, error) {
 	clusterAdmissionPolicy := policiesv1.ClusterAdmissionPolicy{}
 	if err := k8sClient.Get(ctx, client.ObjectKey{Name: name}, &clusterAdmissionPolicy); err != nil {
 		return nil, errors.Join(errors.New("could not find ClusterAdmissionPolicy"), err)
 	}
 	return &clusterAdmissionPolicy, nil
+}
+
+func getTestClusterAdmissionPolicyGroup(ctx context.Context, name string) (*policiesv1.ClusterAdmissionPolicyGroup, error) {
+	clusterAdmissionPolicyGroup := policiesv1.ClusterAdmissionPolicyGroup{}
+	if err := k8sClient.Get(ctx, client.ObjectKey{Name: name}, &clusterAdmissionPolicyGroup); err != nil {
+		return nil, errors.Join(errors.New("could not find ClusterAdmissionPolicyGroup"), err)
+	}
+	return &clusterAdmissionPolicyGroup, nil
 }
 
 func getTestPolicyServer(ctx context.Context, name string) (*policiesv1.PolicyServer, error) {